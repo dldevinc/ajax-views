d99b6dbcf1f0bf3c7eff8ea46cd27dc9eff0d1d5
pix <pix666@ya.ru>
2019-04-28 15:08:35 +0400

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/dldevinc/ajax-views'
 EMAIL = 'x896321475@gmail.com'
 AUTHOR = 'Mihail Mishakin'
 REQUIRES_PYTHON = '>=3.4.0'
-VERSION = '0.2.2'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
